5012a973ea48e3daefb39095c6b15338d456304d
Tachi <Oliver.Neuschl@gmail.com>
2025-02-22 11:47:53 +0100

    URL Util implemented

---
+++ URLUtil/Program.cs
@@ -0,0 +1,165 @@
+﻿using CLIHelper;
+using System.Text;
+
+namespace URLUtil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterArg();
+
+            try
+            {
+                Arguments.ParseArguments(args);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                Console.WriteLine(Generator.GenerateHelp());
+                return;
+            }
+            if (Arguments.IsArgumentSet("help"))
+            {
+                Console.WriteLine(Generator.GenerateHelp());
+                return;
+            }
+            if (Arguments.IsArgumentSet("version"))
+            {
+                Console.WriteLine(Generator.GenerateVersion());
+                return;
+            }
+            if (Arguments.IsArgumentSet("input") && Arguments.IsArgumentSet("inputfile"))
+            {
+                Console.Error.WriteLine("Cannot use both -i and -if");
+                Console.WriteLine(Generator.GenerateHelp());
+                return;
+            }
+            if (!Arguments.IsArgumentSet("encode") && !Arguments.IsArgumentSet("decode"))
+            {
+                Console.Error.WriteLine("Either -e or -d is required");
+                Console.WriteLine(Generator.GenerateHelp());
+                return;
+            }
+
+            if (Arguments.IsArgumentSet("encode") && Arguments.IsArgumentSet("decode"))
+            {
+                Console.Error.WriteLine("Cannot use both -e and -d");
+                Console.WriteLine(Generator.GenerateHelp());
+                return;
+            }
+
+            string input;
+            bool file = Arguments.IsArgumentSet("inputfile");
+            if (!file && Arguments.IsArgumentSet("input"))
+            {
+                input = (string)Arguments.GetArgumentData("input");
+            }
+            else if (file)
+            {
+                input = (string)Arguments.GetArgumentData("inputfile");
+            }
+            else
+            {
+                if (Console.IsInputRedirected || (bool)Arguments.IsArgumentSet("multiline"))
+                {
+                    input = ConsoleExtras.ReadLinesUntilEOF(!Console.IsInputRedirected);
+                }
+                else
+                {
+                    Console.Write("Input> ");
+                    input = Console.ReadLine() ?? "";
+                }
+            }
+
+            if (Arguments.IsArgumentSet("encode"))
+            {
+                byte[] inputData;
+                if (!file)
+                {
+                    inputData = Encoding.UTF8.GetBytes(input);
+                }
+                else
+                {
+                    inputData = File.ReadAllBytes(input);
+                }
+                var output = Encode(inputData);
+                if (Arguments.IsArgumentSet("outputfile"))
+                {
+                    WriteToFile(output);
+                }
+                else
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(output));
+                }
+            }
+            else
+            {
+                try
+                {
+                    byte[] inputdata = Encoding.UTF8.GetBytes(input);
+                    if (file)
+                    {
+                        inputdata = File.ReadAllBytes(input);
+                    }
+                    var output = Decode(inputdata);
+                    if (Arguments.IsArgumentSet("outputfile"))
+                    {
+                        WriteToFile(output);
+                    }
+                    else
+                    {
+                        Console.WriteLine(Encoding.UTF8.GetString(output));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message + "(" + input + ")");
+                }
+            }
+        }
+
+        private static void WriteToFile(byte[] data)
+        {
+            var path = (string)Arguments.GetArgumentData("outputfile");
+            if (File.Exists(path) && !Arguments.IsArgumentSet("overwrite"))
+            {
+                Console.WriteLine($"File {path} already exists. Do you want to override? Y/[N]");
+                var key = Console.ReadKey();
+                if (key.Key != ConsoleKey.Y)
+                {
+                    return;
+                }
+            }
+            File.WriteAllBytes(path, data);
+        }
+
+        private static byte[] Encode(byte[] data)
+        {
+            return System.Web.HttpUtility.UrlEncodeToBytes(data);
+        }
+        private static byte[] Decode(byte[] data)
+        {
+            return System.Web.HttpUtility.UrlDecodeToBytes(data);
+        }
+
+
+        static void RegisterArg()
+        {
+            Config.FullName = "URLUtiil";
+            Config.Version = "0.1.0";
+            Config.License = "Copyright (C) 2025 Oliver Neuschl\r\nThis software uses GPL 3.0 License";
+            Config.HelpHeader = "Base64 Encoder/Decoder";
+            Arguments.RegisterArgument("encode", new ArgumentDefinition(ArgumentType.Flag, "encode", "e", "Encode the URL"));
+            Arguments.RegisterArgument("decode", new ArgumentDefinition(ArgumentType.Flag, "decode", "d", "Decode the URL"));
+            Arguments.RegisterArgument("multiline", new ArgumentDefinition(ArgumentType.Flag, "multiline", "m", "Allow Multiline input when input is not specifed as parameter"));
+            Arguments.RegisterArgument("input", new ArgumentDefinition(ArgumentType.String, "input", "i", "String to encode or decode", "Input Text"));
+            Arguments.RegisterArgument("inputfile", new ArgumentDefinition(ArgumentType.String, "inputfile", "if", "Selects input file (cannot be used with -i)", "File Name"));
+            Arguments.RegisterArgument("outputfile", new ArgumentDefinition(ArgumentType.String, "outputfile", "of", "Selects Output File", "File Name"));
+            Arguments.RegisterArgument("overwrite", new ArgumentDefinition(ArgumentType.Flag, "overwrite", "o", "Overwrite output file if exists"));
+            Arguments.RegisterArgument("help", new ArgumentDefinition(ArgumentType.Flag, "help", "h", "Shows Help"));
+            Arguments.RegisterArgument("version", new ArgumentDefinition(ArgumentType.Flag, "version", "v", "Shows Version"));
+            Config.HelpExample = "-i \"Hello World\" -e -of \"output.txt\"";
+        }
+    }
+}
