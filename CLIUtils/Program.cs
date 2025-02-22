6edf8527d5ea769ebd47d6d55d9d34ce2e059ea1
Tachi <Oliver.Neuschl@gmail.com>
2025-02-22 12:43:22 +0100

    Cleaned up the code

--- CLIUtils/Program.cs
+++ CLIUtils/Program.cs
@@ -22,61 +22,42 @@ namespace CLIUtils
 
             if (Arguments.IsArgumentSet("b64"))
             {
-                var argList = args.ToList();
-                argList.Remove("--base64");
-                argList.Remove("-b64");
-                argList.Remove("/base64");
-                argList.Remove("/b64");
+                args = [.. Arguments.ExtraArguments];
                 Arguments.Reset();
-                Base64.Program.Main([.. argList]);
+                Base64.Program.Main(args);
                 return;
             }
 
             if (Arguments.IsArgumentSet("url"))
             {
-                var argList = args.ToList();
-                argList.Remove("--urlutil");
-                argList.Remove("-url");
-                argList.Remove("/urlutil");
-                argList.Remove("/url");
+                args = [.. Arguments.ExtraArguments];
                 Arguments.Reset();
-                URLUtil.Program.Main([.. argList]);
+                URLUtil.Program.Main(args);
                 return;
             }
 
             if (Arguments.IsArgumentSet("hex"))
             {
-                var argList = args.ToList();
-                argList.Remove("--hexdump");
-                argList.Remove("-hex");
-                argList.Remove("/hexdump");
-                argList.Remove("/hex");
+                args = [.. Arguments.ExtraArguments];
                 Arguments.Reset();
-                HexDump.Program.Main([.. argList]);
-                return;
-            }
-
-            if (Arguments.IsArgumentSet("version"))
-            {
-                Console.WriteLine(Generator.GenerateVersion());
+                HexDump.Program.Main(args);
                 return;
             }
 
             Console.WriteLine(Generator.GenerateHelp());
+            Console.WriteLine(Generator.GenerateVersion());
         }
 
         static void RegisterArg()
         {
-            Config.IgnoreUnknownArguments = true;
+            Config.ErrorOnUnkownArguments = false;
             Config.FullName = "CLUtils Collection";
-            Config.Version = "0.1.0";
+            Config.Version = "1.0.0";
             Config.License = "Copyright (C) 2025 Oliver Neuschl\r\nThis software uses GPL 3.0 License";
             Config.HelpHeader = "CLI Util Collection - Collection of various CLI Utilities";
             Arguments.RegisterArgument("b64", new ArgumentDefinition(ArgumentType.Flag, "base64", "b64", "Base64 Encode/Decode"));
             Arguments.RegisterArgument("url", new ArgumentDefinition(ArgumentType.Flag, "urlutil", "url", "URL Encode/Decode"));
             Arguments.RegisterArgument("hex", new ArgumentDefinition(ArgumentType.Flag, "hexdump", "hex", "Hex Encode/Decode"));
-            Arguments.RegisterArgument("help", new ArgumentDefinition(ArgumentType.Flag, "help", "h", "Shows Help Dialogue"));
-            Arguments.RegisterArgument("version", new ArgumentDefinition(ArgumentType.Flag, "version", "v", "Shows Version"));
         }
     }
 }
